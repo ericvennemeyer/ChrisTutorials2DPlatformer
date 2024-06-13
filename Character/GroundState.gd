03131d192870ed22ce3d869a1627a416fdffa20d
Eric Vennemeyer <82679637+ericvennemeyer@users.noreply.github.com>
2024-06-13 09:58:58 -0700

    Begin tutorial no.3. Create attack animations, empty attack state, and transition code from ground to attack state.

--- Character/GroundState.gd
+++ Character/GroundState.gd
@@ -4,6 +4,8 @@ extends State
 @export var jump_velocity: float = -150.0
 @export var air_state: AirState
 @export var jump_animation: String = "jump"
+@export var attack1_animation: String = "attack1"
+@export var attack_state: AttackState
 
 
 func state_process(delta):
@@ -14,9 +16,16 @@ func state_process(delta):
 func state_input(event: InputEvent):
 	if Input.is_action_just_pressed("jump"):
 		jump()
+	if Input.is_action_just_pressed("attack"):
+		attack()
 
 
 func jump():
 	character.velocity.y = jump_velocity
 	next_state = air_state
 	playback.travel(jump_animation)
+
+
+func attack():
+	playback.travel(attack1_animation)
+	next_state = attack_state
